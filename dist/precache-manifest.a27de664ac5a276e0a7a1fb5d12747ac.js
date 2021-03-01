e86e20adf911f02e2e5cae0c767727acea2cdf42
PaulLCosareanu <paulluciancosareanu@gmail.com>
2021-03-01 15:29:08 +0000

    final version with service worker,manifest, props and emit

---
+++ dist/precache-manifest.a27de664ac5a276e0a7a1fb5d12747ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b4689b496f61e2eaee",
+    "url": "/cw3/css/app.081ceb2b.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/cw3/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "045791566082e8cbf516df89b4cfc84b",
+    "url": "/cw3/index.html"
+  },
+  {
+    "revision": "76b4689b496f61e2eaee",
+    "url": "/cw3/js/app.1c16e795.js"
+  },
+  {
+    "revision": "4e36f4c1ed2270458ffb",
+    "url": "/cw3/js/chunk-vendors.9ea9f1a4.js"
+  },
+  {
+    "revision": "8eff00fb23e5046e8587fb699e79e093",
+    "url": "/cw3/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cw3/robots.txt"
+  }
+]);
